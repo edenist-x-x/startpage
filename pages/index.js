cabed7d9a85cbfa07753535d8991fae86223b247
edenist-x-x <yaron_e5@naver.com>
2021-10-25 21:08:20 +0900

    .

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>STARTPAGE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
